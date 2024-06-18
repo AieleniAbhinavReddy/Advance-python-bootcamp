804a498a5624a0e3d414677486ef1ea41d8c74ef
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2024-06-18 19:45:29 +0530

    made changes in polymorphism

--- Advance python bootcamp/Day 4/PolyMorphism.py
+++ Advance python bootcamp/Day 4/PolyMorphism.py
@@ -1,21 +1,14 @@
 #parent method
 class Parent :
-    #Constructor
-    def __init__(self,name = None,age = None) :
-        self.name = name
-        self.age = age
     def func(self) :
         print('This is Parent class method')
-    def func(self,name) :
-        print(f'My name is {name}')
 #child class
-# class Child :
-#     def func(self) :
-#         print('This is child class method')
+class Child (Parent):
+    def func(self) :
+        print('This is child class method')
 #main method
 def main() :
-    obj = Parent()
+    obj = Child()
     obj.func()
-    obj.func('Abhinav')
 if __name__ == '__main__' :
     main()

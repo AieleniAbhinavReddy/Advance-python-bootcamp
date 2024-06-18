cbd03a5ee8ad8feb3a439e5cbd648b27029cae29
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2024-06-18 12:21:17 +0530

    adding project folder

---
+++ Advance python bootcamp/FInal Project/StudentDB.py
@@ -0,0 +1,19 @@
+'''
+project : student database ( using json or csv )
+
+roles :
+1) student
+2) faculty
+
+features :
+1) add student(s) - only faculty - no duplicate user
+2) update student(s) - based on roll number
+3) get student(s) info - based on roll number
+4) display student(s) details
+5) faculty can see the student placement details
+
+student details :
+1) name
+2) email
+3) roll number
+'''
